c2ffd2bd7fcfd83f834719ccbe0ecf68ad1caaff
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-05-05 21:47:28 +0600

    hide firebase config

--- src/components/Chef.jsx
+++ src/components/Chef.jsx
@@ -10,7 +10,7 @@ const Chef = () => {
             <div className='container mx-auto pb-20'>
                 <h1 className='text-left md:ml-80 pt-10 font-semibold text-4xl mb-6'>Expert chef are here</h1>
                 <div className='grid grid-cols-1 md:grid-cols-3 '>
-                    <div className='col-span-2 grid md:grid-cols-2 gap-6'>
+                    <div className='md:col-span-2 grid md:grid-cols-2 gap-6'>
                         {
                             chefs.map(chef => <ChefCard
                                 key={chef.id}
@@ -40,16 +40,16 @@ const Chef = () => {
                         <div className='mx-10'>
                             <div className='relative mt-20'>
                                 <LazyLoadImage src="https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className='h-48 w-full rounded-md' alt="Image Alt" />
-                                <h1 className='absolute text-emerald-600 bg-slate-100 bottom-0 rounded-xl -left-10 w-96 text-lg p-2'>Roast Chicken With Lemon Gravy</h1>
+                                <h1 className='absolute text-emerald-600 bg-slate-100 bottom-0 rounded-xl md:-left-10 md:w-96 text-lg p-2'>Roast Chicken With Lemon Gravy</h1>
                             </div>
                             <div className='relative mt-10 '>
                                 <LazyLoadImage src="https://images.unsplash.com/photo-1606884285898-277317a7bf12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80" className='h-48 w-full rounded-md' alt="Image Alt" />
-                                <h1 className='absolute text-emerald-600 w-96 bg-slate-100 bottom-0 rounded-xl -left-10  text-lg p-2'>Chocolate Cake With Green Tea Cream</h1>
+                                <h1 className='absolute text-emerald-600 md:w-96 bg-slate-100 bottom-0 rounded-xl md:-left-10  text-lg p-2'>Chocolate Cake With Green Tea Cream</h1>
                             </div>
                             <div className='relative mt-20'>
                                 <LazyLoadImage src="https://images.pexels.com/photos/2673353/pexels-photo-2673353.jpeg?auto=compress&cs=tinysrgb&w=1600" className='h-48 w-full rounded-md' alt="Image Alt" />
 
-                                <h1 className='absolute text-emerald-600 w-96 bg-slate-100 bottom-0 rounded-xl -left-10 text-lg p-2'>Sweet Chilli and Lime Chicken Wings</h1>
+                                <h1 className='absolute text-emerald-600 md:w-96 bg-slate-100 bottom-0 rounded-xl md:-left-10 text-lg p-2'>Sweet Chilli and Lime Chicken Wings</h1>
                             </div>
 
                         </div>
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2yWFPK4NlI6pXhdJPJsrQsQxkGP-05IM",
-  authDomain: "tikkaway-client.firebaseapp.com",
-  projectId: "tikkaway-client",
-  storageBucket: "tikkaway-client.appspot.com",
-  messagingSenderId: "226592733578",
-  appId: "1:226592733578:web:3406c6b3d625096e35f073"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
